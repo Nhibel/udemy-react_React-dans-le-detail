fa94b432f36ffdc63385f37e902d20f5c920de05
Cam <camilledevos@gmail.com>
2021-09-30 11:28:28 +0200

    39. Ne jamais modifier le state directement dans un tableau

--- src/App.js
+++ src/App.js
@@ -3,50 +3,18 @@ import { useState, useEffect, useRef } from "react";
 import Video from "./video.mp4";
 
 function App() {
-  const [toggle, setToggle] = useState(true);
+  const [toggle, setToggle] = useState([1, 2, 3]);
 
   const toggleFunc = () => {
-    setToggle(!toggle);
+    // Ne jamais modifier le state directement, il faut créer un nouvel objet qu'on lui assignera ensuite
+    const newToggle = [...toggle];
+    newToggle.push(newToggle[newToggle.length - 1] + 1);
+    setToggle(newToggle);
   };
-
-  // useRef permet de sélectionner une réf (ici la vidéo)
-  // On le set avec un tableau vide
-  const ref = useRef([]);
-
-  // On fait référence à addToRef depuis le DOM
-  // Permet d'ajouter les multiples références dans un tableau
-  const addToRef = (el) => {
-    if (el && !ref.current.includes(el)) {
-      ref.current.push(el);
-    }
-  };
-
-  // useEffect se "déclenche" après le chargement du DOM, permet ensuite de contrôler la vidéo
-  useEffect(() => {
-    // On lance une action lors d'un resize
-    window.addEventListener("resize", actionResize);
-
-    function actionResize() {
-      console.log("Resized");
-    }
-
-    // On clean lors de la destruction du useEffect (changement de page par exemple)
-    return () => {
-      window.removeEventListener("resize", actionResize);
-    };
-  }, []);
 
   return (
     <div className="App">
-      <video ref={addToRef} width="750" height="500" autoPlay controls muted>
-        <source src={Video} type="video/mp4" />
-      </video>
-      <video ref={addToRef} width="750" height="500" autoPlay controls muted>
-        <source src={Video} type="video/mp4" />
-      </video>
-      <video ref={addToRef} width="750" height="500" autoPlay controls muted>
-        <source src={Video} type="video/mp4" />
-      </video>
+      <h1>{toggle}</h1>
       <button onClick={toggleFunc}>Toggle</button>
     </div>
   );
