f5d6ece5ad2e6c17f217486218410b5e9869e3d8
Cam <camilledevos@gmail.com>
2021-09-30 14:12:13 +0200

    43. Créer un "hook" personnalisé

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import "./App.css";
-import { useState, useEffect, useRef, useMemo } from "react";
-import Content from "./Content";
+import { useState, useEffect, useRef, useMemo, useCallback } from "react";
+import useDimension from "./useDimension";
 
 function App() {
-  const [toggle, setToggle] = useState([1, 2, 3]);
+  const browserWidth = useDimension();
 
-  const toggleFunc = () => {
-    // Ne jamais modifier le state directement, il faut créer un nouvel objet qu'on lui assignera ensuite
-    const newToggle = [...toggle];
-    newToggle.push(newToggle[newToggle.length - 1] + 1);
-    setToggle(newToggle);
-  };
+  if (browserWidth > 722) {
+    console.log("grand écran");
+  } else {
+    console.log("petit écran");
+  }
 
-  const tableau = useMemo(() => {
-    return [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  }, []);
-
-  return (
-    <div className="App">
-      <Content num={tableau} />
-      <button onClick={toggleFunc}>Toggle</button>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
