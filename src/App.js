4678a6fdaf5f03bda809832883dcd5cd22bb6c2e
Cam <camilledevos@gmail.com>
2021-09-30 16:45:48 +0200

    44. Créer une fenêtre modale

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import "./App.css";
-import { useState, useEffect, useRef, useMemo, useCallback } from "react";
-import useDimension from "./useDimension";
+import Modal from "./Components/Modal/Modal";
 
 function App() {
-  const browserWidth = useDimension();
-
-  if (browserWidth > 722) {
-    console.log("grand écran");
-  } else {
-    console.log("petit écran");
-  }
-
-  return <div className="App"></div>;
+  return (
+    <>
+      <Modal />
+    </>
+  );
 }
 
 export default App;
