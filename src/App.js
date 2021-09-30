144c0f62460a02c59b6c46069986dbd9ea49463b
Cam <camilledevos@gmail.com>
2021-09-30 10:50:13 +0200

    36. Le hook "useRef()"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-import Timer from "./Timer";
+import { useState, useEffect, useRef } from "react";
+import Video from "./video.mp4";
 
 function App() {
   const [toggle, setToggle] = useState(true);
@@ -9,10 +9,22 @@ function App() {
     setToggle(!toggle);
   };
 
+  // useRef permet de sélectionner une réf (ici la vidéo)
+  const ref = useRef();
+
+  // useEffect se "déclenche" après le chargement du DOM, permet ensuite de contrôler la vidéo
+  useEffect(() => {
+    setTimeout(() => {
+      ref.current.pause();
+    }, 1500);
+  }, []);
+
   return (
     <div className="App">
+      <video ref={ref} width="750" height="500" autoPlay controls muted>
+        <source src={Video} type="video/mp4" />
+      </video>
       <button onClick={toggleFunc}>Toggle</button>
-      <h1>{toggle && <Timer />}</h1>
     </div>
   );
 }
