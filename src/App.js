6eabbc026f7ddfed3a5e757478a74d97f82770d9
Cam <camilledevos@gmail.com>
2021-10-01 14:20:16 +0200

    46. Coder une navbar responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Modal from "./Components/Modal/Modal";
 import Accord from "./Components/Accord/Accord";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       {/* <Modal /> */}
-      <Accord />
+      {/* <Accord /> */}
+      <Navbar />
     </>
   );
 }
